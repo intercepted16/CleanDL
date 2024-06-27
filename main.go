ad13ab81f3ede11091be1ea5afa0b9a8e06bf442
intercepted16 <162676516+intercepted16@users.noreply.github.com>
2024-06-27 17:11:33 +1000

    feat: partially implement custom patterns support

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -9,31 +11,115 @@ import (
 	"time"
 )
 
-// Define the source directory (Downloads folder)
-func getDownloadsFolder() string {
+func getUserFolder() string {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		panic(err)
 	}
-	return filepath.Join(homeDir, "Downloads")
+	return homeDir
+}
+
+// Define the source directory (Downloads folder)
+func getDownloadsFolder() string {
+	return filepath.Join(getUserFolder(), "Downloads")
 }
 
 // Define the file types/regex patterns, their corresponding age thresholds (in days), destination folders, and deletion flag
-var fileTypesAndInfo = map[string]struct {
-	ageThreshold int
-	destination  string
-	deleteFlag   bool
-}{
-	".pdf":                      {14, filepath.Join(os.Getenv("USERPROFILE"), "OneDrive/Documents"), false},
-	".reg":                      {0, `C:\bin\reg`, false},
-	".msi":                      {0, `C:\bin\msi`, false},
-	`.*(Installer|Setup)\.exe$`: {14, "", true},
-	`.*Tool\.exe$`:              {0, `C:\bin\exe`, false},
-	// Add more patterns, thresholds, folders, and deletion flags as needed
+var defaultPatterns = regexPatternsJSON{
+	Patterns: regexPatterns{
+		".pdf": {
+			AgeThreshold: 14,
+			Destination:  filepath.Join(os.Getenv("USERPROFILE"), "OneDrive/Documents"),
+			DeleteFlag:   false,
+		},
+		".reg": {
+			AgeThreshold: 0,
+			Destination:  `C:\bin\reg`,
+			DeleteFlag:   false,
+		},
+		".msi": {
+			AgeThreshold: 0,
+			Destination:  `C:\bin\msi`,
+			DeleteFlag:   false,
+		},
+		`.*(Installer|Setup)\.exe$`: {
+			AgeThreshold: 14,
+			Destination:  "",
+			DeleteFlag:   true,
+		},
+		`.*Tool\.exe$`: {
+			AgeThreshold: 0,
+			Destination:  `C:\bin\exe`,
+			DeleteFlag:   false,
+		},
+		// Add more patterns, thresholds, folders, and deletion flags as needed
+	},
+}
+
+type regexInfo struct {
+	AgeThreshold int
+	Destination  string
+	DeleteFlag   bool
+}
+
+type regexPatternsJSON struct {
+	Patterns regexPatterns
+}
+
+type regexPatterns map[string]regexInfo
+
+var fileTypesAndInfoPath = filepath.Join(getUserFolder(), "AppData", "Local", "CleanDL", "patterns.json")
+
+func createSettings(path string) {
+	// Ensure the directory exists
+	dir := filepath.Dir(path)
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+		panic(err)
+	}
+
+	// if the file doesn't exist, create it
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		file, err := os.Create(path)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+
+		// Serialize the map to JSON
+		jsonData, err := json.Marshal(defaultPatterns)
+		if err != nil {
+			panic(err) // Consider more graceful error handling
+		}
+
+		// Write the JSON data to the file
+		if _, err := file.Write(jsonData); err != nil {
+			panic(err) // Consider more graceful error handling
+		}
+	}
+}
+
+func getSettings(path string) regexPatterns {
+	settingsFile, err := os.Open(path)
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Successfully Opened %s\n", path)
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer settingsFile.Close()
+	byteValue, _ := io.ReadAll(settingsFile)
+	// we initialize our custom regex array
+	var regexPatternsJSON regexPatternsJSON
+
+	// we unmarshal our byteArray which contains our
+	// jsonFile's content into 'users' which we defined above
+	json.Unmarshal(byteValue, &regexPatternsJSON)
+	var regexPatterns regexPatterns = regexPatternsJSON.Patterns
+	return regexPatterns
 }
 
 // Function to move or delete files based on age, type, and regex or simple string
-func processFiles(downloadsFolder string) {
+func processFiles(patterns regexPatterns, downloadsFolder string) {
 	files, err := os.ReadDir(downloadsFolder)
 	if err != nil {
 		panic(err)
@@ -49,7 +135,7 @@ func processFiles(downloadsFolder string) {
 		}
 		fileAgeDays := int(currentTime.Sub(fileInfo.ModTime()).Hours() / 24)
 
-		for pattern, info := range fileTypesAndInfo {
+		for pattern, info := range patterns {
 			matched := false
 			if strings.HasSuffix(pattern, "$") {
 				// Regex pattern match
@@ -63,15 +149,15 @@ func processFiles(downloadsFolder string) {
 			}
 
 			if matched {
-				if fileAgeDays > info.ageThreshold {
+				if fileAgeDays > info.AgeThreshold {
 					fmt.Printf("File age: %d", fileAgeDays)
-					fmt.Printf("Age threshold: %d", info.ageThreshold)
-					if info.deleteFlag {
+					fmt.Printf("Age threshold: %d", info.AgeThreshold)
+					if info.DeleteFlag {
 						os.Remove(filePath) // Delete the file
 						fmt.Printf("Deleted: %s\n", filePath)
-					} else if info.destination != "" {
-						os.Rename(filePath, filepath.Join(info.destination, file.Name())) // Move the file
-						fmt.Printf("Moved: %s to %s\n", filePath, info.destination)
+					} else if info.Destination != "" {
+						os.Rename(filePath, filepath.Join(info.Destination, file.Name())) // Move the file
+						fmt.Printf("Moved: %s to %s\n", filePath, info.Destination)
 					}
 					break // Exit the loop after processing
 				}
@@ -81,8 +167,10 @@ func processFiles(downloadsFolder string) {
 }
 
 func main() {
+	createSettings(fileTypesAndInfoPath)
+	patterns := getSettings(fileTypesAndInfoPath)
 	downloadsFolder := getDownloadsFolder()
 	print("Processing files in: ", downloadsFolder, "\n")
-	processFiles(downloadsFolder)
+	processFiles(patterns, downloadsFolder)
 	print("\nDone!", "\n")
 }
